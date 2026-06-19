ee33c98f49b9bcf351029b3a93943e8cad9858dc
gitanubhavtiwari <anubhavtiwarioffice@gmail.com>
2026-06-19 17:07:42 +0530

    Time: 10 ms (99.53%) | Memory: 97.9 MB (6.25%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet <Integer> set = new HashSet<>();
+
+
+        for(int x : nums){
+
+            if(!set.add(x)){
+                return true;
+                
+            }
+
+            
+        }
+        return false ;
+    
+    }
+
+}
